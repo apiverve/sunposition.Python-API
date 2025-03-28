009e432a31e712f82ac5566be4122ab7bfdcada8
APIVerve Team <hello@apiverve.com>
2025-03-28 17:54:01 -0500

    Bug Fixes

--- src/apiverve_sunposition/setup.py
+++ src/apiverve_sunposition/setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_sunposition',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
-    description='Sun Position is a simple tool for getting sun position data. It returns data such as sunrise, sunset, solar noon, and more based on the location provided.',
+    install_requires=install_requires,
+    description='Sun Position is a simple tool for getting sun position data. It returns data such as altitude, azimuth, and distance of the sun from the location provided.',
     author='APIVerve',
     author_email='hello@apiverve.com',
     url='https://apiverve.com',
